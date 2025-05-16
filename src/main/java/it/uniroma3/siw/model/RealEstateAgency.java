24c816046bfa83f99e9c25b2ea61316740284c4b
Sparkimuz <t.cenciotti@gmail.com>
2025-05-16 16:05:29 +0200

    Modello realestate classe aggiunta

---
+++ src/main/java/it/uniroma3/siw/model/RealEstateAgency.java
@@ -0,0 +1,49 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+public class RealEstateAgency {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@OneToMany(mappedBy = "realEstateAgency", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+	List<Agent> agents;
+	
+	private String address;
+	private String city;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public List<Agent> getAgents() {
+		return agents;
+	}
+	public void setAgents(List<Agent> agents) {
+		this.agents = agents;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+
+}
