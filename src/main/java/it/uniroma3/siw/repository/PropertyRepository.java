1c3b5e47464edc63e314d6aef3b3224e8f6141a5
AndreaAvadanii <andreaavadanii@gmail.com>
2025-07-24 12:56:35 +0200

    Piccole modifiche

--- src/main/java/it/uniroma3/siw/repository/PropertyRepository.java
+++ src/main/java/it/uniroma3/siw/repository/PropertyRepository.java
@@ -11,7 +11,6 @@ import it.uniroma3.siw.model.Property;
 public interface PropertyRepository extends CrudRepository<Property, Long>{
 
 	public Property findByCity(String city);
-	public Property findByType(String type);
 	public boolean existsByAddressAndCity(String address, String city);
 	public Property findByAddressAndCity(String address, String city);
 	public List<Property> findByAgent(Agent agent);
